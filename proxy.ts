86db3d1c5fa02bdf5b12d71299c1f5bae57969e9
Rafael Menegalli <rafaelmenegalli@gmail.com>
2026-01-27 14:41:40 -0300

    altera o middleware para proxy para seguir o padrão recomendado do next

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { authCookie, verifyAuthToken } from "./lib/auth";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     const token = request.cookies.get(authCookie.name)?.value;
 
     if (!token) {
